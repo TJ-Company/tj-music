f90a364aa96ab11f9bdcd017e97ac2f732237bbe
wenatt <sidorenivan@mail.ru>
2026-04-15 22:21:24 +0300

    Test deleted

--- src/test/java/ru/tjcomp/music/MusicApplicationTests.java
+++ src/test/java/ru/tjcomp/music/MusicApplicationTests.java
@@ -3,14 +3,11 @@ package ru.tjcomp.music;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 @SpringBootTest
 class MusicApplicationTests {
 
 	@Test
 	void contextLoads() {
-		assertEquals(1, 2);
 	}
 
 }
